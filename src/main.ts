e1ec06d7a238cad2daaf2269668006ce51d19dd7
Jaan Jahilo <jaanjahilo@outlook.com>
2023-02-08 19:47:51 +0200

    Improve test file

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,14 @@ import QuaverClient from './QuaverClient';
 
 const client = new QuaverClient();
 
-client.users.getUsers({ id: [23, 24] }).then((res) => {
-	console.log('getUsers', JSON.stringify(res, null, 2));
-});
+async function main() {
+	// log('getUsers', await client.users.getUsers({ id: [23, 24] }));
+	// log('getUserById', await client.users.getUserById(23));
+	log('searchUserByName', await client.users.searchUserByName('Progress'));
+}
 
-client.users.getUserById(23).then((res) => {
-	console.log('getUserById', JSON.stringify(res, null, 2));
-});
+function log(name: string, input: object) {
+	console.log(name, JSON.stringify(input, null, 2));
+}
+
+main().then();
